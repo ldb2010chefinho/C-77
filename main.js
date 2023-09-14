36df8b95032f4bb07e00a78503a33d38de523186
ldb2010chefinho <128753023+ldb2010chefinho@users.noreply.github.com>
2023-09-14 19:28:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Freddy";
+var last_name= "Fazbear";
+var concat = first_name.concat(last_name);
+console.log(concat.concat(" e um animatronic"));
+
+var numero1 = 12;
+var numero2 = 53;
+console.log(numero1 + numero2);
+
+
+console.log(numero1 - numero2);
+
+
+console.log(numero1 * numero2);
